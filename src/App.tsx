0677f8a539e4df789b5136ae2fd0122ba1671be6
charles <87439476+RobinCharles984@users.noreply.github.com>
2022-04-11 11:24:43 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ReactDOM } from 'react'
+import Header from './components/Header'
+
+export function App(){
+    return(
+        <div>
+            <Header />
+        </div>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+    <h1>Editor!</h1>
+}
+
+export default Header;
