b97bb1267e8cb2e5079cec80c1e16477be1545e5
charles <87439476+RobinCharles984@users.noreply.github.com>
2022-04-18 11:34:20 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { ReactDOM } from 'react'
-import Header from './components/Header'
 
 export function App(){
     return(
         <div>
-            <Header />
+            <h1>Hello World!</h1>
         </div>
     )
 }
---
+++ src/services/Firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCSrSAOCn8ml84MIcMW_CX2fJStBDZnM04",
+    authDomain: "saturno-editor-65965.firebaseapp.com",
+    projectId: "saturno-editor-65965",
+    storageBucket: "saturno-editor-65965.appspot.com",
+    messagingSenderId: "266686794251",
+    appId: "1:266686794251:web:669decbb311f5daafa150e"
+};
+
+const firebase = initializeApp(firebaseConfig);
